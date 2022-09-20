c18929d9e2c51d35d999032c0f1f949aa011f214
joserot <jose.rotchen14@gmail.com>
2022-09-20 15:51:24 -0300

    page postulantes

--- src/components/postulantes/PostulantesCards.js
+++ src/components/postulantes/PostulantesCards.js
@@ -3,15 +3,30 @@ import ubiImg from "../../../public/img/ubi-img.png";
 import person1Img from "../../../public/img/person-1.png";
 import person2Img from "../../../public/img/person-2.png";
 import person3Img from "../../../public/img/person-3.png";
+import { useState } from "react";
 
 const PostulantesCards = () => {
+	const [drop, setDrop] = useState("Recientes");
+
+	const changeDrop = (e) => {
+		const value = e.target.textContent;
+		setDrop(value);
+	};
 	return (
 		<section className="postulantes-cards">
 			<div className="postulantes-cards-nav">
 				<h3>Ver 22 candidatos</h3>
-				<select>
-					<option>Reciente</option>
-				</select>
+				<span class="dropdown">
+					<button>{drop} </button>
+					<label>
+						<input type="checkbox" />
+						<ul>
+							<li onClick={changeDrop}>Recientes</li>
+							<li onClick={changeDrop}>Una opción</li>
+							<li onClick={changeDrop}>Otra opción</li>
+						</ul>
+					</label>
+				</span>
 			</div>
 			<div className="postulantes-cards-cards">
 				<article className="card-postulante">
--- src/components/postulantes/PostulantesForm.js
+++ src/components/postulantes/PostulantesForm.js
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import searchIcon from "../../../public/img/search-2.png";
+import { useState } from "react";
 
 const PostulantesForm = () => {
+	const [drop, setDrop] = useState("Recientes");
+
+	const changeDrop = (e) => {
+		const value = e.target.textContent;
+		setDrop(value);
+	};
 	return (
 		<form className="postulantes-form">
 			<div>
@@ -9,13 +16,29 @@ const PostulantesForm = () => {
 				<input placeholder="Buscar por nombre" type="text" />
 			</div>
 			<div className="separator"></div>
-			<select>
-				<option>Ubicaciones</option>
-			</select>
+			<span class="dropdown">
+				<button>Ubicaciones </button>
+				<label>
+					<input type="checkbox" />
+					<ul>
+						<li onClick={changeDrop}>Opción 1</li>
+						<li onClick={changeDrop}>Opción 2</li>
+						<li onClick={changeDrop}>Opción 3</li>
+					</ul>
+				</label>
+			</span>
 			<div className="separator"></div>
-			<select>
-				<option>Área o Rubro</option>
-			</select>
+			<span class="dropdown">
+				<button>Área o Rubro </button>
+				<label>
+					<input type="checkbox" />
+					<ul>
+						<li onClick={changeDrop}>Opción 1</li>
+						<li onClick={changeDrop}>Opción 2</li>
+						<li onClick={changeDrop}>Opción 3</li>
+					</ul>
+				</label>
+			</span>
 			<button>Encontrar candidatos</button>
 		</form>
 	);
